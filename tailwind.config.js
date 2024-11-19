47ef54a9eb825a1689bb0830c076c6cd8a47c8fe
Abdulrahman Al-Hmeeede <abdulrahmanhmeedi@gmail.com>
2024-11-20 01:43:18 +0300

    init tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "color-primary": "#4CAF4F",
+        "color-secondary": "#4d4d4d",
+        "color-accent": "#717171",
+        "color-section": "#f5f7fa",
+        "color-card": "#abbed166",
+        "color-footer": "#263238",
+        "color-text-nav": "#18191F",
+        "color-white": "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
